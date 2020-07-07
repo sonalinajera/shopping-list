d23f043681f6185c1341a19b6885164968185715
Sonali Najera <sonalitzul@gmail.com>
2020-07-07 16:01:25 -0700

    cleaned up js code presentation

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 'use strict';
 function shoppingList() {
-    // add items to list when you have text and you click add item
-    // should inherit css properties defined by li 
-    $('form').on('submit', function (event) {
-        event.preventDefault();
-        // need to wrap in if statment when done
-        let currentInput = $('#shopping-list-entry').val();
-        // {} run function of adding to list
-        if (currentInput !== '') {
-            $(".shopping-list").append(renderItem(currentInput));
-        } else {
-            alert('Enter shopping item!')
-        }
-    });
+  // add items to list when you have text and you click add item
+  // should inherit css properties defined by li 
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    // need to wrap in if statment when done
+    let currentInput = $('#shopping-list-entry').val();
+    // {} run function of adding to list
+    if (currentInput !== '') {
+      $(".shopping-list").append(renderItem(currentInput));
+    } else {
+      alert('Enter shopping item!')
+    }
+  });
 
-    // alternatively have a render function 
-    function renderItem(text) {
-        return `<li>
+  // alternatively have a render function to style item added to list
+  function renderItem(text) {
+    return `<li>
     <span class="shopping-item">${text}</span>
     <div class="shopping-item-controls">
       <button class="shopping-item-toggle">
@@ -27,20 +27,19 @@ function shoppingList() {
       </button>
     </div>
   </li>`
-    };
+  };
 
-    // toggle  item between strike through and no text decoration when button 'check' is clicked
-    $('ul').on('click', '.shopping-item-toggle', function (event) {
-        // if list first first child's class doesn't have "shopping-item__checked" add 
-        //else if it does have "shopping-item__checked" remove
-        console.log(event.target);
-        $(event.target).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
+  // toggle  item between strike through and no text decoration when button 'check' is clicked
+  $('ul').on('click', '.shopping-item-toggle', function (event) {
+    // if first list item with class shopping item class doesn't have "shopping-item__checked" add 
+    //else if it does have "shopping-item__checked" remove this class 
+    $(event.target).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 
-    // should delete item from list when delete button clicked 
-    $('ul').on('click', '.shopping-item-delete', function (event) {
-        $(this).closest('li').remove();
-    });
+  // should delete item from list when delete button clicked 
+  $('ul').on('click', '.shopping-item-delete', function (event) {
+    $(this).closest('li').remove();
+  });
 
 
 }
